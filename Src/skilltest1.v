df02f5e14eb784cfd44593861402c3d047e668d8
Pitchaya Sitthi-amorn <pitchaya@gmail.com>
2025-02-28 14:19:19 +0700

    Makeup exam

---
+++ Src/skilltest1.v
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+module skilltest1 (
+    input  wire       Clk,
+    input  wire       Reset,
+    input  wire [3:0] Trigger,
+    output wire [3:0] BCD0,
+    output wire [3:0] BCD1,
+    output wire [3:0] BCD2,
+    output wire [3:0] BCD3
+);
+
+endmodule
